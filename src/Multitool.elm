23adf0c9cd550a589ceafe545d42203796206699
Ed Kelly <edwardjkelly@gmail.com>
2026-04-21 16:26:28 +0100

    Add a bunch more primitives and combinators

--- src/Multitool.elm
+++ src/Multitool.elm
@@ -1,4 +1,29 @@
-module Multitool exposing (..)
+module Multitool exposing
+    ( bool
+    , char
+    , custom
+    , dict
+    , endCustom
+    , endRecord
+    , field
+    , float
+    , int
+    , list
+    , map
+    , maybe
+    , record
+    , result
+    , set
+    , string
+    , triple
+    , tuple
+    , variant0
+    , variant1
+    , variant2
+    , variant3
+    , variant4
+    , variant5
+    )
 
 
 int =
@@ -11,6 +36,71 @@ bool =
 
 string =
     .string
+
+
+float =
+    .float
+
+
+char =
+    .char
+
+
+list =
+    .list
+
+
+dict =
+    .dict
+
+
+set =
+    .set
+
+
+maybe a =
+    custom
+        (\nothing_ just_ variant ->
+            case variant of
+                Nothing ->
+                    nothing_
+
+                Just a_ ->
+                    just_ a_
+        )
+        |> variant0 "Nothing" Nothing
+        |> variant1 "Just" Just a
+        |> endCustom
+
+
+result x a =
+    custom
+        (\err_ ok_ variant ->
+            case variant of
+                Err x_ ->
+                    err_ x_
+
+                Ok a_ ->
+                    ok_ a_
+        )
+        |> variant1 "Err" Err x
+        |> variant1 "Ok" Ok a
+        |> endCustom
+
+
+tuple fst snd =
+    record Tuple.pair
+        |> field "First" Tuple.first fst
+        |> field "Second" Tuple.second snd
+        |> endRecord
+
+
+triple fst snd thd =
+    record (\fst_ snd_ thd_ -> ( fst_, snd_, thd_ ))
+        |> field "First" (\( fst_, _, _ ) -> fst_) fst
+        |> field "Second" (\( _, snd_, _ ) -> snd_) snd
+        |> field "Third" (\( _, _, thd_ ) -> thd_) thd
+        |> endRecord
 
 
 custom match =
@@ -33,6 +123,24 @@ variant2 name ctor arg1 arg2 prev =
     \adapter ->
         prev adapter
             |> adapter.variant2 name ctor (arg1 adapter) (arg2 adapter)
+
+
+variant3 name ctor arg1 arg2 arg3 prev =
+    \adapter ->
+        prev adapter
+            |> adapter.variant3 name ctor (arg1 adapter) (arg2 adapter) (arg3 adapter)
+
+
+variant4 name ctor arg1 arg2 arg3 arg4 prev =
+    \adapter ->
+        prev adapter
+            |> adapter.variant4 name ctor (arg1 adapter) (arg2 adapter) (arg3 adapter) (arg4 adapter)
+
+
+variant5 name ctor arg1 arg2 arg3 arg4 arg5 prev =
+    \adapter ->
+        prev adapter
+            |> adapter.variant5 name ctor (arg1 adapter) (arg2 adapter) (arg3 adapter) (arg4 adapter) (arg5 adapter)
 
 
 endCustom prev =
@@ -61,9 +169,3 @@ map f prev =
     \adapter ->
         prev adapter
             |> adapter.map f
-
-
-andThen f prev =
-    \adapter ->
-        prev adapter
-            |> adapter.andThen (\v -> f adapter v)
